6f1df49c6165ce3f9f8210fa6da18153fff74b52
Padmapiyush Pathak <padmpiyush2801@gmail.com>
2026-04-26 06:47:23 +0530

    Set browser tab icon to custom wolf logo

--- components/Command.js
+++ components/Command.js
@@ -1,10 +1,14 @@
 import Input from "./Input";
 import Output from "./Output";
 
-export default function Command({ command, output, onSubmit }) {
+export default function Command({ command, output, onSubmit, suggestions = [] }) {
   return (
     <div>
-      <Input command={command} onSubmit={(command) => onSubmit(command)} />
+      <Input
+        command={command}
+        onSubmit={(command) => onSubmit(command)}
+        suggestions={suggestions}
+      />
       {output && <Output output={output} />}
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/wolf-logo.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/wolf-logo.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
